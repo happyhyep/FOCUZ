a6c1c7e5c6f4d3c82e6db3e9f074a6937656a649
hyemimi <hyemi7375@gmail.com>
2022-08-23 21:16:21 +0900

    [더미데이터 적용]

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ import { Routes, Route } from 'react-router-dom';
 import routes from './components/Common/Routes';
 import { fetchUserInfo } from './store/user';
 import { useThemeColor } from './Context/theme';
+import { tempUser } from './store/temp/tempUserData';
 
 function App() {
     const theme = useThemeColor();
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [user, setUser] = useState(null);
+    const [isLoggedIn, setIsLoggedIn] = useState(true); // false로 수정해야함 (8/23)
+    const [user, setUser] = useState(tempUser); // null로 수정해야함 (8/23)
 
     useEffect(() => {
         fetchUserInfo(2).then((res) => {
